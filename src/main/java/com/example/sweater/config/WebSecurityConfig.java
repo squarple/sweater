7a38d29adffe5c5df96256d76b71078a8083e692
Anton Radionov <radionoff.antony@gmail.com>
2021-06-11 13:49:47 +0300

    "/activate/*" url was added to the security configuration.

--- src/main/java/com/example/sweater/config/WebSecurityConfig.java
+++ src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -21,7 +21,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/registration", "/static/**").permitAll()
+                //одна * - только один сегмент после; две ** - все урлы
+                    .antMatchers("/", "/registration", "/static/**", "/activate/*").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
